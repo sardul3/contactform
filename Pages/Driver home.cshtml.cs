e37996b5766b80ecf7d7ab838f3197113d7eb02c
unknown <poudelsagar530@gmail.com>
2021-04-11 11:21:52 -0500

    initial commit

---
+++ Pages/Driver home.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tiger_Ride.Pages
+{
+    public class Driver_homeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
